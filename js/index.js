723a9045149220489709b264406d44bea9149077
Nina Le <nina_le91@hotmail.com>
2016-09-12 17:34:51 +1000

    change doc ready tag

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
- $(document).ready(){
-   alert("Hello! I am an alert box!!");
-   $('#show-this-on-click').addClass('hide');
+ $(document).ready(start);
+
+ function start(){
+   $('#show-this-on-click').addClass('.hide');
+ }
 
    $('.readmore').on('click', function() {
      $(this).next('#show-this-on-click').toggleClass('.hide');
-   });
-}]
+   })
